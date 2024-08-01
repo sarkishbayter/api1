aa1528f88db8ec37ec3ff22edac1a4128fdf26f1
sarkis hbayter <sarkishbayter112233@gmail.com>
2024-08-01 11:20:52 +0300

    c1

---
+++ index.js
@@ -0,0 +1,19 @@
+var express = require('express')
+var cors = require('cors')
+var app = express()
+app.use(cors())
+const PORT = process.env.PORT || 3030;
+
+var xlsx=require('xlsx');
+let wb=xlsx.readFile('students.xlsx');
+let ws=wb.Sheets["Sheet1"];
+let data=xlsx.utils.sheet_to_json(ws);
+
+app.listen(PORT, () => {
+  console.log(`server started on port ${PORT}`);
+});
+
+app.get('/', function (req, res) {
+  res.json(data)
+});
+ 
---
+++ notes.js
@@ -0,0 +1,18 @@
+// github can only be used to put html,css and js
+// we cannot put api on github 
+// heroku --> online site that can i put my files on it (but its paied )
+// render.com --> its for free that help me to put my api online 
+// static files --> html,css,js and photos (can be put it in render.com) (same as github)
+//web services == api == web api 
+// host== deploy my api online 
+// meaning port : bineyet 3enda ip sebett w fiya che2a2 w kell che2a 3enda ra2em m3ayyan hayda l ra2em houwe l port 
+// --> there is no 2 api running on the same port and the same machine 
+// ana mafinne ejbour render enno bade he l port --> henne herrin 
+// there is a file  named : .env (bass bhett bi albou esas chakla name:value)(we put the conf entries)(lezem koun mnazal l dotenv)
+// .env --> ana bekhla2o bel folder li fetho bel code vs ta yekhoud mennou l port l dotenv 
+// npm i dotenv  (  use require('dotenv').config();  or dotenv=require('dotenv');  )
+//for exp :  PORT=3030
+//           rita=rizk
+// hatta law mawjoud 3ende port bel .env l render malah yestaamlo la2an houwe 3endo wehed la elo --> dont need to dotenv 
+
+
